034751e15d8ef390e81f952c4a6fdb438508df27
Thomas Zhou <thethomaszhou@gmail.com>
2020-04-13 23:18:25 -0700

    Testing out the tutorial

--- public/app.js
+++ public/app.js
@@ -1,56 +1,64 @@
 // DOM Elements
-
-const darkButton = document.getElementById('dark');
 const lightButton = document.getElementById('light');
+const darkButton = document.getElementById('dark');
 const solarButton = document.getElementById('solar');
 const body = document.body;
 
-
 // Apply the cached theme on reload
-
 const theme = localStorage.getItem('theme');
 const isSolar = localStorage.getItem('isSolar');
-
 if (theme) {
-  body.classList.add(theme);
-  isSolar && body.classList.add('solar');
+  body.className = theme;
+}
+if (isSolar) {
+  body.classList.add('solar');
+  solarButton.style.cssText = `
+    --bg-solar: white;
+  `
+  solarButton.innerText = 'normalize';
 }
 
 // Button Event Handlers
+lightButton.onclick = () => {
+  body.classList.replace('dark', 'light');
+  localStorage.setItem('theme', 'light');
+  if (!body.classList.contains('solar')) {
+    solarButton.style.cssText = `
+      --bg-solar: var(--yellow);
+    `
+  }
+}
 
 darkButton.onclick = () => {
   body.classList.replace('light', 'dark');
   localStorage.setItem('theme', 'dark');
-};
-
-lightButton.onclick = () => {
-  body.classList.replace('dark', 'light');
-
-  localStorage.setItem('theme', 'light');
-};
+  if (!body.classList.contains('solar')) {
+    solarButton.style.cssText = `
+      --bg-solar: var(--blue);
+    `
+  }
+}
 
 solarButton.onclick = () => {
+  body.classList.toggle('solar');
 
   if (body.classList.contains('solar')) {
-    
-    body.classList.remove('solar');
-    solarButton.style.cssText = `
-      --bg-solar: var(--yellow);
-    `
-
-    solarButton.innerText = 'solarize';
-
-    localStorage.removeItem('isSolar');
-
-  } else {
-
     solarButton.style.cssText = `
       --bg-solar: white;
     `
-
-    body.classList.add('solar');
     solarButton.innerText = 'normalize';
-
     localStorage.setItem('isSolar', true);
+  } else {
+    if (body.classList.contains('light')) {
+      solarButton.style.cssText = `
+        --bg-solar: var(--yellow);
+      `
+    } else if (body.classList.contains('dark')) {
+      solarButton.style.cssText = `
+        --bg-solar: var(--blue);
+      `
+    }
+    solarButton.innerText = 'solarize';
+    localStorage.removeItem('isSolar');
   }
-};
+}
